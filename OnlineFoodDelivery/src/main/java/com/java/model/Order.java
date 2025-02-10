6c090d393468698a44f50d510181f8817738d7f8
archig97 <archismitaghosh@yahoo.com>
2025-02-10 15:35:34 -0500

    Order entity is ready

--- OnlineFoodDelivery/src/main/java/com/java/model/Order.java
+++ OnlineFoodDelivery/src/main/java/com/java/model/Order.java
@@ -1,16 +1,44 @@
 package com.java.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToOne
+    private User customer;
+    @JsonIgnore //prevent large payload and data exposure
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderstatus;
+
+    private Date createdAt;
+    @ManyToOne
+    private Address deliveryAddress;
+    @OneToMany
+    private List<OrderItem> items;
+    private int totalItems;
+    private double totalPrice;
+    //private Payment payment;
+
+
+
 
 
 
---
+++ OnlineFoodDelivery/src/main/java/com/java/model/OrderItem.java
@@ -0,0 +1,19 @@
+package com.java.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
